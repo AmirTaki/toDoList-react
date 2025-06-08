03c5b58fe501f9a468a4e6884ed1ee33cee5dc7f
amir <amirtaki623@gmail.com>
2025-06-09 02:55:59 +0330

    Marking Todo Items As Complete

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -2,13 +2,19 @@ import styles from "./todoitem.module.css";
 
 export default function TodoItem({item, todoList, setTodoList}){
     function handleDelete (item){
-        console.log(todoList)
         setTodoList(todoList.filter((todo)=> todo !== item))
     }
+    function handelClick (name){
+        const NewArray = todoList.map((x)=> x.name === name ? {...x, done : !x.done } : x);
+        setTodoList(NewArray)
+    }
+    const className =  item.done ? styles.completed : "";
     return (
         <div className={styles.item}> 
             <div className = {styles.itemName}>
-                {item.name}
+                <span onClick={()=>handelClick(item.name)}  className={className}>  
+                    {item.name}
+                </span>
                 <span>
                     <button onClick={()=>handleDelete(item)} className = {styles.deleteButton}>x</button>
                 </span>    
